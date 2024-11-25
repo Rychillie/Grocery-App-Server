1b68d44caca3ccf6c5212ef67ec97eba26ec3266
Rychillie <rychillie@rychillie.net>
2024-11-25 19:04:12 -0300

    Deleting Grocery Category (Server)

--- Sources/App/Controllers/GroceryController.swift
+++ Sources/App/Controllers/GroceryController.swift
@@ -23,6 +23,35 @@ class GroceryController: RouteCollection {
         // GET: /api/users/:userId/grocery-categories
         api.get("grocery-categories", use: getGroceryCategoriesByUser)
         
+        // DELETE: /api/users/:userId/grocery-categories/:groceryCategoryId
+        api.delete("grocery-categories", ":groceryCategoryId", use: deleteGroceryCategory)
+        
+    }
+    
+    func deleteGroceryCategory(req: Request) async throws -> GroceryCategoryResponseDTO {
+        
+        guard let userId = req.parameters.get("userId", as: UUID.self),
+              let groceryCategoryId = req.parameters.get("groceryCategoryId", as: UUID.self)
+        else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let groceryCategory = try await GroceryCategory.query(on: req.db)
+            .filter(\.$user.$id == userId)
+            .filter(\.$id == groceryCategoryId)
+            .first() else {
+                throw Abort(.notFound)
+            }
+        
+        
+        try await groceryCategory.delete(on: req.db)
+        
+        guard let groceryCategoryResponseDTO = GroceryCategoryResponseDTO(groceryCategory) else {
+            throw Abort(.internalServerError)
+        }
+        
+        return groceryCategoryResponseDTO
+        
     }
     
     func getGroceryCategoriesByUser(req: Request) async throws -> [GroceryCategoryResponseDTO] {
@@ -46,7 +75,11 @@ class GroceryController: RouteCollection {
         
         let groceryCategoryRequestDTO = try req.content.decode(GroceryCategoryRequestDTO.self)
         
-        let groceryCategory = GroceryCategory(title: groceryCategoryRequestDTO.title, colorCode: groceryCategoryRequestDTO.colorCode, userId: userId)
+        let groceryCategory = GroceryCategory(
+            title: groceryCategoryRequestDTO.title,
+            colorCode: groceryCategoryRequestDTO.colorCode,
+            userId: userId
+        )
         
         try await groceryCategory.save(on: req.db)
         
