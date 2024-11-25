7e9e4e82d612fe0c86dfe713d0f601ec7d90d1da
Rychillie <rychillie@rychillie.net>
2024-11-24 21:45:57 -0300

    Create Package for Login and Register Response DTO

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "4.2.2"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+        .package(url: "https://github.com/Rychillie/GroceryAppSharedDTO.git", branch: "main")
     ],
     targets: [
         .executableTarget(
@@ -22,7 +23,8 @@ let package = Package(
                 .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "JWT", package: "jwt"),
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "GroceryAppSharedDTO", package: "GroceryAppSharedDTO")
             ],
             swiftSettings: swiftSettings
         ),
---
+++ Sources/App/Extensions/LoginResponseDTO+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  LoginResponseDTO+Extensions.swift
+//  grocery-app-server
+//
+//  Created by Rychillie Umpierre de Oliveira on 24/11/24.
+//
+
+import Foundation
+import GroceryAppSharedDTO
+import Vapor
+
+extension LoginResponseDTO: Content {
+    
+}
