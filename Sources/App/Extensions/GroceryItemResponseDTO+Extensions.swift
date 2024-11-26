2720b0b49cf18034cdcb16e0dc8d29baf21f9ce2
Rychillie <rychillie@rychillie.net>
2024-11-26 14:35:16 -0300

    Saving Grocery Item

---
+++ Sources/App/Extensions/GroceryItemResponseDTO+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  GroceryItemResponseDTO+Extensions.swift
+//  grocery-app-server
+//
+//  Created by Rychillie Umpierre de Oliveira on 26/11/24.
+//
+
+import Foundation
+import Vapor
+import GroceryAppSharedDTO
+
+extension GroceryItemResponseDTO: Content {
+    init?(_ groceryItem: GroceryItem) {
+        guard let groceryItemId = groceryItem.id else {
+            return nil
+        }
+        
+        self.init(
+            id: groceryItemId,
+            title: groceryItem.title,
+            price: groceryItem.price,
+            quantity: groceryItem.quantity
+        )
+    }
+}
